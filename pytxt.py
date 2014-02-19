ee68a5ee1854397fc682c3889e29e8e6a897d708
Jake Schmitz <jake.d.schmitz@gmail.com>
2014-02-19 12:31:17 -0800

    added some comments

--- pytxt.py
+++ pytxt.py
@@ -1,8 +1,11 @@
 import sys
 
-
+#  storing stdin in the var buffy and then spliting it by spaces
 buffy = sys.stdin.read()
 buffy = buffy.split(' ')
+
+#  this function will capitalize everything in buffy join it together. And print
+#  it out
 
 
 def capAll():
@@ -10,11 +13,17 @@ def capAll():
     result = ' '.join(result)
     print result
 
+#  this function will lower everything in buffy and then join it together and
+#  print the result
+
 
 def lowAll():
     result = [x.lower() for x in buffy if x != '']
     result = ' '.join(result)
     print result
+
+#  this will capitalize the specified letter (via numerical position) in each
+#  word in buffy and print it out
 
 
 def capLetter(n):
@@ -23,6 +32,9 @@ def capLetter(n):
     result = [x[:n] + x[n].upper() + x[m:] for x in buffy if x != '' and len(x) > m]
     result = ' '.join(result)
     print result
+
+#  this will lowercase the specified letter (via numerical position) in each
+#  work in buffy and print it out
 
 
 def lowLetter(n):
