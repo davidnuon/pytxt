402cb8f82e540b965b1c6f49ef92d527943d26a6
Jake Schmitz <jake.d.schmitz@gmail.com>
2014-02-24 10:25:06 -0800

    basic error handling added

--- clipboard.py
+++ clipboard.py
@@ -7,13 +7,19 @@ os = sys.platform
 def getClip():
     #  gets the operating system
     if os == 'darwin':
-        p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
-        buffy = p.stdout.read()
-        return buffy
+        try:
+            p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
+            buffy = p.stdout.read()
+            return buffy
+        except OSError:
+            print 'Xcode is not installed'
     elif os == 'linux2':
-        p = subprocess.Popen(['xsel --clipboard --output'], stdout=subprocess.PIPE)
-        buffy = p.stdout.read()
-        return buffy
+        try:
+            p = subprocess.Popen(['xsel --clipboard --output'], stdout=subprocess.PIPE)
+            buffy = p.stdout.read()
+            return buffy
+        except OSError:
+            print 'xsel must be installed'
     elif os == 'win32':
         p = Tk()
         buffy = p.selection_get(selection='CLIPBOARD')
@@ -23,13 +29,19 @@ def getClip():
 def setClip(data):
     #  gets the operating system
     if os == 'darwin':
-        p = subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE)
-        p.stdin.write(data)
-        p.stdin.close()
+        try:
+            p = subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE)
+            p.stdin.write(data)
+            p.stdin.close()
+        except OSError:
+            print 'Xcode is not installed'
     elif os == 'linux2':
-        p = subprocess.Popen(['xsel --clipboard --input'], stdin=subprocess.PIPE)
-        p.stdin.write(data)
-        p.stdin.close()
+        try:
+            p = subprocess.Popen(['xsel --clipboard --input'], stdin=subprocess.PIPE)
+            p.stdin.write(data)
+            p.stdin.close()
+        except OSError:
+            print 'xsel must be installed'
     elif os == 'win32':
         p = Tk()
         p.clipboard_clear()
